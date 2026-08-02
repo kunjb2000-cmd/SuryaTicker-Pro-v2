594312dc3874a3500a2eee1a6e0b03233150ebe8
kunjb2000-cmd <kunjb2000@gmail.com>
2026-08-02 18:37:08 +0530

    Add createBox function for dynamic overlays

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
 // SuryaTicker Pro V2
+
+const canvas = document.getElementById("canvas");
+
+function createBox(text, x, y) {
+
+    const box = document.createElement("div");
+
+    box.className = "overlay";
+
+    box.innerHTML = text;
+
+    box.style.left = x + "px";
+
+    box.style.top = y + "px";
+
+    canvas.appendChild(box);
+
+}
+
+document.getElementById("addLive").onclick = () => {
+
+    createBox("🔴 LIVE",100,100);
+
+};
+
+document.getElementById("addTicker").onclick = () => {
+
+    createBox("Scrolling Text",100,200);
+
+};
+
+document.getElementById("addLogo").onclick = () => {
+
+    createBox("LOGO",100,300);
+
+};
+
+document.getElementById("addClock").onclick = () => {
+
+    createBox("12:00 PM",100,400);
+
+};
